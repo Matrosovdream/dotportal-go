1ed1e69deb0204a4fe8ca1a87383b086791f77d6
Stan Matrosov <matrosovdream@gmail.com>
2025-06-28 00:48:23 +0200

    Auth handler, initial version

---
+++ internal/handlers/auth_handler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func Register(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Welcome to the Dot Portal API11!"}`))
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message":"Welcome to the Dot Portal API22!"}`))
+}
