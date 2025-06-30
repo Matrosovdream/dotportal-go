eaf1f768593ff5af53d2193b76079172c6425336
Stan Matrosov <matrosovdream@gmail.com>
2025-06-30 22:17:12 +0200

    Router, new migration methods

--- internal/routes/router.go
+++ internal/routes/router.go
@@ -18,6 +18,10 @@ func NewRouter() *mux.Router {
   // Login
   r.HandleFunc("/login", handlers.Login).Methods("POST")
 
+  // Migrations
+  r.HandleFunc("/migrate-up", handlers.MigrateUp).Methods("GET")
+  r.HandleFunc("/migrate-down", handlers.MigrateDown).Methods("GET")
+
 	return r
 
 }
