477bfedd474f024d53538563511c0ca47027db73
Stan Matrosov <matrosovdream@gmail.com>
2025-06-30 22:17:36 +0200

    New migration handler

--- internal/handlers/migration_handler.go
+++ internal/handlers/migration_handler.go
@@ -17,7 +17,7 @@ func MigrateUp(w http.ResponseWriter, r *http.Request) {
 	// Respond with success	
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"Migration endpoint is not implemented yet."}`))
+	w.Write([]byte(`{"message":"Migration has been done successfully."}`))
 }
 
 // MigrateDown handles the rollback request
@@ -32,5 +32,5 @@ func MigrateDown(w http.ResponseWriter, r *http.Request) {
 	// Respond with success
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message":"Rollback endpoint is not implemented yet."}`))
+	w.Write([]byte(`{"message":"Migration rollback is successful."}`))
 }
